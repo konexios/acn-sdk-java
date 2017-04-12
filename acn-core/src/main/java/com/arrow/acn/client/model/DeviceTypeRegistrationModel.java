8eabf277d2f9df1ddf708062bb563052932b848e
Tam Nguyen <tnguyen@arrow.com>
2017-04-11 21:22:58 -0500

    added metadata

--- acn-core/src/main/java/com/arrow/acn/client/model/DeviceTypeRegistrationModel.java
+++ acn-core/src/main/java/com/arrow/acn/client/model/DeviceTypeRegistrationModel.java
@@ -11,107 +11,122 @@
 package com.arrow.acn.client.model;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class DeviceTypeRegistrationModel implements Serializable {
+	private static final long serialVersionUID = 3174634854949378263L;
 
-    private static final long serialVersionUID = 7441457283950048558L;
+	private String name;
+	private String description;
+	private List<DeviceTypeTelemetryModel> telemetries;
+	private Map<String, DeviceStateValueMetadataModel> stateMetadata = new HashMap<>();
+	private boolean enabled = true;
 
-    private String name;
-    private String description;
-    private List<DeviceTypeTelemetryModel> telemetries;
-    private boolean enabled = true;
+	public void trim() {
+		name = StringUtils.trimToNull(name);
+		description = StringUtils.trimToNull(description);
+	}
 
-    public void trim() {
-        name = StringUtils.trimToNull(name);
-        description = StringUtils.trimToNull(description);
-    }
+	public DeviceTypeRegistrationModel withName(String name) {
+		setName(name);
+		return this;
+	}
 
-    public DeviceTypeRegistrationModel withName(String name) {
-        setName(name);
-        return this;
-    }
+	public DeviceTypeRegistrationModel withDescription(String name) {
+		setDescription(name);
+		return this;
+	}
 
-    public DeviceTypeRegistrationModel withDescription(String name) {
-        setDescription(name);
-        return this;
-    }
+	public DeviceTypeRegistrationModel withEnabled(boolean enabled) {
+		setEnabled(enabled);
+		return this;
+	}
 
-    public DeviceTypeRegistrationModel withEnabled(boolean enabled) {
-        setEnabled(enabled);
-        return this;
-    }
+	public DeviceTypeRegistrationModel withTelemetries(List<DeviceTypeTelemetryModel> telemetries) {
+		setTelemetries(telemetries);
+		return this;
+	}
 
-    public DeviceTypeRegistrationModel withTelemetries(List<DeviceTypeTelemetryModel> telemetries) {
-        setTelemetries(telemetries);
-        return this;
-    }
+	public DeviceTypeRegistrationModel withStateMetadata(Map<String, DeviceStateValueMetadataModel> stateMetadata) {
+		setStateMetadata(stateMetadata);
+		return this;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public List<DeviceTypeTelemetryModel> getTelemetries() {
-        return telemetries;
-    }
+	public List<DeviceTypeTelemetryModel> getTelemetries() {
+		return telemetries;
+	}
 
-    public void setTelemetries(List<DeviceTypeTelemetryModel> telemetries) {
-        this.telemetries = telemetries;
-    }
+	public void setTelemetries(List<DeviceTypeTelemetryModel> telemetries) {
+		this.telemetries = telemetries;
+	}
 
-    public boolean isEnabled() {
-        return enabled;
-    }
+	public boolean isEnabled() {
+		return enabled;
+	}
 
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        result = prime * result + (enabled ? 1231 : 1237);
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
+	public Map<String, DeviceStateValueMetadataModel> getStateMetadata() {
+		return stateMetadata;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        DeviceTypeRegistrationModel other = (DeviceTypeRegistrationModel) obj;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        if (enabled != other.enabled)
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
-    }
+	public void setStateMetadata(Map<String, DeviceStateValueMetadataModel> stateMetadata) {
+		this.stateMetadata = stateMetadata;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + (enabled ? 1231 : 1237);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DeviceTypeRegistrationModel other = (DeviceTypeRegistrationModel) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (enabled != other.enabled)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
