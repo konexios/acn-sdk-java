bfbb78a1f61f674a8678b032653eb2295a057228
Inga Gladkova <ingladk@mera.ru>
2017-07-18 14:08:58 +0300

    KR-2094: added ConfigBackup models

---
+++ acn-core/src/main/java/com/arrow/acn/client/model/ConfigBackupModel.java
@@ -0,0 +1,83 @@
+package com.arrow.acn.client.model;
+
+import com.arrow.acs.client.model.TsModelAbstract;
+
+public class ConfigBackupModel extends TsModelAbstract<ConfigBackupModel> {
+
+	private static final long serialVersionUID = 6313590525521543445L;
+
+	public enum Type {
+		DEVICE, GATEWAY
+	}
+
+	private String name;
+	private Type type;
+	private String objectHid;
+	private DeviceConfigBackupModel deviceConfig;
+	private GatewayConfigBackupModel gatewayConfig;
+
+	@Override
+	protected ConfigBackupModel self() {
+		return this;
+	}
+
+	public ConfigBackupModel withName(String name) {
+		setName(name);
+		return this;
+	}
+
+	public ConfigBackupModel withType(Type type) {
+		setType(type);
+		return this;
+	}
+
+	public ConfigBackupModel withDeviceConfig(DeviceConfigBackupModel deviceConfig) {
+		setDeviceConfig(deviceConfig);
+		return this;
+	}
+
+	public ConfigBackupModel withGatewayConfig(GatewayConfigBackupModel gatewayConfig) {
+		setGatewayConfig(gatewayConfig);
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public String getObjectHid() {
+		return objectHid;
+	}
+
+	public void setObjectHid(String objectHid) {
+		this.objectHid = objectHid;
+	}
+
+	public DeviceConfigBackupModel getDeviceConfig() {
+		return deviceConfig;
+	}
+
+	public void setDeviceConfig(DeviceConfigBackupModel deviceConfig) {
+		this.deviceConfig = deviceConfig;
+	}
+
+	public GatewayConfigBackupModel getGatewayConfig() {
+		return gatewayConfig;
+	}
+
+	public void setGatewayConfig(GatewayConfigBackupModel gatewayConfig) {
+		this.gatewayConfig = gatewayConfig;
+	}
+}
---
+++ acn-core/src/main/java/com/arrow/acn/client/model/GatewayConfigBackupModel.java
@@ -0,0 +1,96 @@
+package com.arrow.acn.client.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.arrow.acn.client.model.GatewayModel.GatewayType;
+
+public class GatewayConfigBackupModel extends BaseDeviceConfigBackupModel {
+
+	private static final long serialVersionUID = -5455425739418112548L;
+
+	private GatewayType type;
+	private String osName;
+	private String softwareName;
+	private String softwareVersion;
+	private String sdkVersion;
+	private List<ConfigurationPropertyModel> configurations = new ArrayList<>();
+
+	public GatewayConfigBackupModel withType(GatewayType type) {
+		setType(type);
+		return this;
+	}
+
+	public GatewayConfigBackupModel withOsName(String osName) {
+		setOsName(osName);
+		return this;
+	}
+
+	public GatewayConfigBackupModel withSoftwareName(String softwareName) {
+		setSoftwareName(softwareName);
+		return this;
+	}
+
+	public GatewayConfigBackupModel withSoftwareVersion(String softwareVersion) {
+		setSoftwareVersion(softwareVersion);
+		return this;
+	}
+
+	public GatewayConfigBackupModel withSdkVersion(String sdkVersion) {
+		setSdkVersion(sdkVersion);
+		return this;
+	}
+
+	public GatewayConfigBackupModel withConfigurations(List<ConfigurationPropertyModel> configurations) {
+		setConfigurations(configurations);
+		return this;
+	}
+
+	public GatewayType getType() {
+		return type;
+	}
+
+	public void setType(GatewayType type) {
+		this.type = type;
+	}
+
+	public String getOsName() {
+		return osName;
+	}
+
+	public void setOsName(String osName) {
+		this.osName = osName;
+	}
+
+	public String getSoftwareName() {
+		return softwareName;
+	}
+
+	public void setSoftwareName(String softwareName) {
+		this.softwareName = softwareName;
+	}
+
+	public String getSoftwareVersion() {
+		return softwareVersion;
+	}
+
+	public void setSoftwareVersion(String softwareVersion) {
+		this.softwareVersion = softwareVersion;
+	}
+
+	public String getSdkVersion() {
+		return sdkVersion;
+	}
+
+	public void setSdkVersion(String sdkVersion) {
+		this.sdkVersion = sdkVersion;
+	}
+
+	public List<ConfigurationPropertyModel> getConfigurations() {
+		return configurations;
+	}
+
+	public void setConfigurations(List<ConfigurationPropertyModel> configurations) {
+		this.configurations = configurations;
+	}
+}
